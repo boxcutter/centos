b88422e070019cbd7a1adb54fdda8a793e781228
Mischa Taylor <mischa@misheska.com>
2014-05-31 05:47:51 -0700

    Adding test to verify .vbox_version file left behind

--- test/centos_spec.rb
+++ test/centos_spec.rb
@@ -4,4 +4,12 @@ describe 'box' do
   it 'should have a root user' do
     expect(user 'root').to exist
   end
+
+  it 'should have a vagrant user' do
+    expect(user 'vagrant').to exist
+  end
+
+  it 'should not have a .vbox_version file' do
+    expect(file '/home/vagrant/.vbox_version').to_not be_file
+  end
 end
