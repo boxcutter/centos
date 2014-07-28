232e164bed4fe20beccc77b7b2a5170ebb4a5f73
Mischa Taylor <mischa@misheska.com>
2014-07-28 15:11:04 -0700

    Patching VMware 9.6.2 tools so shared folders work on CentOS 7

--- script/vmtool.sh
+++ script/vmtool.sh
@@ -3,17 +3,39 @@
 install_vmware_tools_centos_70()
 {
     cd /tmp
-    git clone https://github.com/rasa/vmware-tools-patches.git
     mkdir -p /mnt/cdrom
     mount -o loop /home/vagrant/linux.iso /mnt/cdrom
-    cp /mnt/cdrom/VMwareTools-*.tar.gz /tmp/vmware-tools-patches
-    cd /tmp/vmware-tools-patches
-    ./untar-and-patch-and-compile.sh
+    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+
+    mkdir -p /mnt/floppy
+    modprobe floppy
+    mount -t vfat /dev/fd0 /mnt/floppy
+
+    cd /tmp/vmware-tools-distrib
+
+    if [[ -f /mnt/cdrom/VMwareTools-9.6.2-1688356.tar.gz ]]
+    then
+        pushd lib/modules/source
+        if [ ! -f vmhgfs.tar.orig ]
+        then
+            cp vmhgfs.tar vmhgfs.tar.orig
+        fi
+        rm -rf vmhgfs-only
+        tar xf vmhgfs.tar
+        pushd vmhgfs-only/shared
+        patch -p1 < /mnt/floppy/vmware9.compat_dcache.h.patch
+        popd
+        tar cf vmhgfs.tar vmhgfs-only
+        rm -rf vmhgfs-only
+        popd        
+    fi
+
+    /tmp/vmware-tools-distrib/vmware-install.pl --default
     rm /home/vagrant/linux.iso
     umount /mnt/cdrom
     rmdir /mnt/cdrom
     rm -rf /tmp/VMwareTools-*
-    rm -rf /tmp/vmware-tools-patches
+
     exit
 }
 
