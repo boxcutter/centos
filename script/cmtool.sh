ee417951d001867e2c7ce1066c9bf64091be63ba
Mischa Taylor <mischa@misheska.com>
2014-08-04 19:39:33 -0700

    Adding -k (insecure) option to curl, which works behind a http proxy

--- script/cmtool.sh
+++ script/cmtool.sh
@@ -27,10 +27,10 @@ install_chef()
     echo "==> Installing Chef"
     if [[ ${CM_VERSION:-} == 'latest' ]]; then
         echo "==> Installing latest Chef version"
-        curl -L https://www.opscode.com/chef/install.sh | sh
+        curl -Lk https://www.opscode.com/chef/install.sh | sh
     else
         echo "==> Installing Chef version ${CM_VERSION}"
-        curl -L https://www.opscode.com/chef/install.sh | sh -s -- -v ${CM_VERSION}
+        curl -Lk https://www.opscode.com/chef/install.sh | sh -s -- -v ${CM_VERSION}
     fi
     if [[ ${CM_SET_PATH:-} == 'true' ]]; then
       echo "Automatically setting vagrant PATH to Chef Client"
@@ -43,10 +43,10 @@ install_chefdk()
     echo "==> Installing Chef Development Kit"
     if [[ ${CM_VERSION:-} == 'latest' ]]; then
         echo "==> Installing latest Chef version"
-        curl -L https://www.opscode.com/chef/install.sh | sh -s -- -P chefdk
+        curl -Lk https://www.opscode.com/chef/install.sh | sh -s -- -P chefdk
     else
         echo "==> Installing Chef version ${CM_VERSION}"
-        curl -L https://www.opscode.com/chef/install.sh | sh -s -- -P chefdk -v ${CM_VERSION}
+        curl -Lk https://www.opscode.com/chef/install.sh | sh -s -- -P chefdk -v ${CM_VERSION}
     fi
     if [[ ${CM_SET_PATH:-} == 'true' ]]; then
       echo "Automatically setting vagrant PATH to Chef Client"
