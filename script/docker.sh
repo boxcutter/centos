36ca6ede8145d2aecf84578d31fe43cf2df16456
Mischa Taylor <mischa@misheska.com>
2014-08-09 13:15:33 -0700

    Fixing typo in script

--- script/docker.sh
+++ script/docker.sh
@@ -6,6 +6,7 @@ if grep -q -i "release 7" /etc/redhat-release ; then
     rpm -Uvh http://mirrors.mit.edu/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
 elif grep -q -i "release 6" /etc/redhat-release ; then
     rpm -Uvh http://fedora-epel.mirror.lstn.net/6/i386/epel-release-6-8.noarch.rpm
+fi
 
 echo "==> Installing docker"
 yum install -y docker-io
