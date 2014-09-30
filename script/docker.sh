cfc068f1900b0e56e7ac974b5639007445e857ec
Mischa Taylor <mischa@misheska.com>
2014-09-30 05:10:24 -0700

    Updating epel link - changing to wget for better error handling

--- script/docker.sh
+++ script/docker.sh
@@ -3,11 +3,13 @@
 echo "==> Adding EPEL repo"
 cat /etc/redhat-release
 if grep -q -i "release 7" /etc/redhat-release ; then
-    rpm -Uvh http://mirrors.mit.edu/epel/beta/7/x86_64/epel-release-7-0.2.noarch.rpm
+    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+    rpm -Uvh epel-release-7*.rpm
     echo "==> Installing docker"
     yum install -y docker
 elif grep -q -i "release 6" /etc/redhat-release ; then
-    rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    rpm -Uvh epel-release-6*.rpm
     echo "==> Installing docker"
     yum install -y docker-io
 fi
