c86f9e8264819a74f4d884cdda4ea9a70dc4530e
Mischa Taylor <mischa@misheska.com>
2015-01-13 18:44:36 -0800

    Streamline installation of epel repo

--- script/docker.sh
+++ script/docker.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 echo "==> Adding EPEL repo"
+yum install -y epel-release
+cat /etc/resolv.conf
 cat /etc/redhat-release
 if grep -q -i "release 7" /etc/redhat-release ; then
-    wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-    rpm -Uvh epel-release-7*.rpm
     echo "==> Installing docker"
     yum install -y docker
 elif grep -q -i "release 6" /etc/redhat-release ; then
-    wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    rpm -Uvh epel-release-6*.rpm
     echo "==> Installing docker"
     yum install -y docker-io
 fi
