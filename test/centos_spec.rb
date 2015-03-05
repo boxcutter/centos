47802a3c3f61b3c2f96df4c23f2bc3b012b3e451
Moujan Anna <moujan@annawake.com>
2015-03-05 07:06:00 -0800

    Add test to verify that vendor-supplied openssl certs are being used

--- test/centos_spec.rb
+++ test/centos_spec.rb
@@ -24,4 +24,11 @@ describe 'box' do
       expect(file('/etc/resolv.conf').content).to match /single-request-reopen/
     end
   end
+
+  # https://www.chef.io/blog/2015/02/26/bento-box-update-for-centos-and-fedora/
+  describe 'test-cacert' do
+    it 'uses the vendor-supplied openssl certificates' do
+      expect(command('openssl s_client -CAfile /etc/pki/tls/certs/ca-bundle.crt -connect packagecloud-repositories.s3.amazonaws.com:443 </dev/null 2>&1 | grep -i "verify return code"').stdout).to match /\s+Verify return code: 0 \(ok\)/
+    end
+  end
 end
