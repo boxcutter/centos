263657773834d4f087051ae66cc829b723e31da5
Moujan Anna <moujan@annawake.com>
2015-03-05 07:06:36 -0800

    Add test to verify that the vagrant user is a member of the docker group

--- test/centos_spec.rb
+++ test/centos_spec.rb
@@ -31,4 +31,9 @@ describe 'box' do
       expect(command('openssl s_client -CAfile /etc/pki/tls/certs/ca-bundle.crt -connect packagecloud-repositories.s3.amazonaws.com:443 </dev/null 2>&1 | grep -i "verify return code"').stdout).to match /\s+Verify return code: 0 \(ok\)/
     end
   end
+
+  has_docker = command('command -v docker').exit_status == 0
+  it 'should make vagrant a member of the docker group', :if => has_docker do
+    expect(user 'vagrant').to belong_to_group 'docker'
+  end
 end
