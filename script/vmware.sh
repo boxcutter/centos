e506669e7670320073904bcabf31718e2122e102
Moujan Anna <moujan@annawake.com>
2016-08-27 14:03:23 -0700

    Phase out cmtool build, use custom-script.sh instead

--- script/vmware.sh
+++ script/vmware.sh
@@ -3,6 +3,48 @@
 SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
+function install_open_vm_tools {
+    echo "==> Installing Open VM Tools"
+    # Install open-vm-tools so we can mount shared folders
+    yum install -y open-vm-tools
+    # Add /mnt/hgfs so the mount works automatically with Vagrant
+    mkdir /mnt/hgfs
+}
+
+function install_vmware_tools {
+    echo "==> Installing VMware Tools"
+    # Assuming the following packages are installed
+    # apt-get install -y linux-headers-$(uname -r) build-essential perl
+
+    cd /tmp
+    mkdir -p /mnt/cdrom
+    mount -o loop /home/${SSH_USERNAME}/linux.iso /mnt/cdrom
+
+    VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
+    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
+    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
+    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
+    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
+    echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
+    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}"
+
+    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
+    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
+        /tmp/vmware-tools-distrib/vmware-install.pl -d
+    else
+        /tmp/vmware-tools-distrib/vmware-install.pl -f
+    fi
+
+    rm /home/${SSH_USERNAME}/linux.iso
+    umount /mnt/cdrom
+    rmdir /mnt/cdrom
+    rm -rf /tmp/VMwareTools-*
+
+    VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
+    echo "==> Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}"
+}
+
 if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
     echo "==> Installing VMware Tools"
     cat /etc/redhat-release
@@ -21,30 +63,11 @@ if [[ $PACKER_BUILDER_TYPE =~ vmware ]]; then
         echo "export PATH=$PATH:/usr/sbin:/sbin" >> $SSH_USER_HOME/.bashrc
     fi
 
-    cd /tmp
-    mkdir -p /mnt/cdrom
-    mount -o loop $SSH_USER_HOME/linux.iso /mnt/cdrom
-
-    VMWARE_TOOLS_PATH=$(ls /mnt/cdrom/VMwareTools-*.tar.gz)
-    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
-    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
-    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
-    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
-    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
-    echo "==> VMware Tools Version: ${VMWARE_TOOLS_VERSION}"
-    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}"
-
-    tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-
-    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
-        /tmp/vmware-tools-distrib/vmware-install.pl -d
+    if [ "${MAJOR_VERSION}" -ge "4" ] && [ "${MINOR_VERSION}" -ge "1" ]; then
+      install_open_vm_tools
     else
-        /tmp/vmware-tools-distrib/vmware-install.pl -f
+      install_vmware_tools
     fi
-    rm $SSH_USER_HOME/linux.iso
-    umount /mnt/cdrom
-    rmdir /mnt/cdrom
-    rm -rf /tmp/VMwareTools-*
 
     echo "==> Removing packages needed for building guest tools"
     yum -y remove gcc cpp libmpc mpfr kernel-devel kernel-headers
