1c1222cc1033a440661ef30820127eacd9026104
Moujan Anna <moujan@annawake.com>
2016-11-10 09:11:34 -0800

    Parallels 12 tools install now requires bash

--- script/parallels.sh
+++ script/parallels.sh
@@ -6,7 +6,7 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 if [[ $PACKER_BUILDER_TYPE =~ parallels ]]; then
     echo "==> Installing Parallels tools"
     mount -o loop $SSH_USER_HOME/prl-tools-lin.iso /mnt
-    sh /mnt/install --install-unattended-with-deps
+    bash /mnt/install --install-unattended-with-deps
     umount /mnt
     rm -rf $SSH_USER_HOME/prl-tools-lin.iso
     rm -f $SSH_USER_HOME/.prlctl_version
